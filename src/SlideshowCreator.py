81946b798064db71f0e86629635ab127835695f0
jxr814 <jxr814@bham.ac.uk>
2019-02-28 19:19:19 +0000

    Fixed style

--- src/SlideshowCreator.py
+++ src/SlideshowCreator.py
@@ -2,6 +2,7 @@ from pprint import pprint
 import Image
 import Slide
 
+
 def fitness(a, b):
     a_tags = a.tags
     b_tags = b.tags
@@ -48,7 +49,8 @@ def construct_slideshow(slides):
 
     matrix = []
     #  Calculate a fitness matrix for all slides.
-    #  TODO: this is inefficient as the matrix is symmetrical, so you don't need to compute it all
+    #  TODO: this is inefficient as the matrix is symmetrical, so you don't
+    #  need to compute it all
     for i in range(0, len(slides)):
         for j in range(0, len(slides)):
             matrix[i][j] = fitness(slides[i], slides[j])
@@ -56,8 +58,10 @@ def construct_slideshow(slides):
     #  Choose pairs of slides that maximise the fitness.
     pairs = []
 
-    #  Select the pair with the worst left-hand slide and choose that as the starting slide
+    #  Select the pair with the worst left-hand slide and choose that as the
+    #  starting slide
 
-    #  Piece the rest of the slides together by selecting the best slide for the current right-hand slide
+    #  Piece the rest of the slides together by selecting the best slide for
+    #  the current right-hand slide
 
     #  Return the constructed slideshow
